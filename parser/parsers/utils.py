c6acd05006f9587ee04fdd072448c6cd6a974038
mjekson <mjeksonmjekson@gmail.com>
2025-11-18 10:37:24 +0300

    fix decimal

--- parser/parsers/utils.py
+++ parser/parsers/utils.py
@@ -5,5 +5,8 @@ Debezium encodes decimal values as  {scale: scale, value: "base64 value"}
 import base64
 
 
-def decode_decimal(obj: dict) -> int:
-    return int.from_bytes(base64.b64decode(obj['value']), 'big') / pow(10, obj['scale'])
+def decode_decimal(obj) -> int:
+    if isinstance(obj, dict):
+        return int.from_bytes(base64.b64decode(obj['value']), 'big') / pow(10, obj['scale'])
+    # If already a string or int, just convert to int
+    return int(obj)
